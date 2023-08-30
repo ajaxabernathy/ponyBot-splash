26e13298af1c82d28ee86f6722bce8cf7a7889c8
ajaxabernathy <ajax.abernathy@gmail.com>
2023-08-30 19:20:35 -0400

    Finished landing page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c7d54aeb044aab06"; }
+/******/ 		__webpack_require__.h = function() { return "1b9ca535e44ade96"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ components/features-card-two.tsx
@@ -0,0 +1,14 @@
+export default function FeaturesCardTwo() {
+  return (
+    <section className='w-full h-screen bg-black text-white'>
+      <div>
+        <h2>Avoid Overkill</h2>
+        <p className='flex w-1/2 justify-center border-2'>
+          Traditional setups involving Prometheus and Grafana can be
+          resource-intensive and complex, especially when the need is for simple
+          yet crucial data about the cluster's health and performance.
+        </p>
+      </div>
+    </section>
+  );
+}
